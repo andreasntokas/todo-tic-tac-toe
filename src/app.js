313fed75a563818068e6ad8431be9a6344fc16a6
Andreas Ntokas <ntokas.an@gmail.com>
2022-05-21 16:09:30 +0300

    WIP

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
+import './style.css';
+
 // VARIABLES
 
+const projectEleme = document.getElementById('project');
 const circleWinsElem = document.getElementById('circle-wins');
 const xWinsElem = document.getElementById('x-wins');
 const drawCountElem = document.getElementById('draws');
 const boardElem = document.getElementById('board');
+const appElem = document.querySelector('.app');
 
-const turn = document.getElementById('turn');
+const displayCurrentPlayer = document.getElementById('displayCurrentPlayer');
 const xDisplay = document.getElementById('x-turn');
 const circleDisplay = document.getElementById('circle-turn');
+const refreshButton = document.querySelector('.refresh');
 
 // STATE
 
@@ -17,6 +22,9 @@ const state = {
   drawCount: 0,
   board: ['', '', '', '', '', '', '', '', ''],
   currentPlayer: 'X',
+  turnCount: 0,
+  winner: '',
+  displayCurrentPlayer: '',
 };
 
 const winningCombination = [
@@ -30,34 +38,216 @@ const winningCombination = [
   [2, 4, 6],
 ];
 
-function addX(e) {
-  const cell = e.target;
-  const addxx = document.createElement('h3');
-  addxx.classList.add('addxx');
-  addxx.innerHTML = '<i class="fa-solid fa-x"></i>';
-  cell.style.backgroundColor = 'white';
-  cell.style.cursor = 'default';
-  cell.appendChild(addxx);
-}
-
-function addCircle(e) {
-  const cell = e.target;
-  const addcirclee = document.createElement('h3');
-  addcirclee.classList.add('addcirclee');
-  addcirclee.innerHTML = '<i class="fa-solid fa-o"></i>';
-  cell.style.backgroundColor = 'white';
-  cell.style.cursor = 'default';
-  cell.appendChild(addcirclee);
-}
-
 function clear(element) {
   element.innerHTML = '';
 }
 
-function nextMove(cell) {
-  state.board[cell] = state.currentPlayer;
-  state.currentPlayer = state.currentPlayer === 'X' ? 'O' : 'X';
-  console.log(state);
+function roundWinner() {
+  // 0-1-2
+  if (
+    state.board[0] === 'X' &&
+    state.board[1] === 'X' &&
+    state.board[2] === 'X'
+  ) {
+    state.board = ['', '', '', '', '', '', '', '', ''];
+    state.currentPlayer = 'X';
+    state.turnCount = 0;
+    state.xWins += 1;
+    render(state);
+  } else if (
+    state.board[0] === 'O' &&
+    state.board[1] === 'O' &&
+    state.board[2] === 'O'
+  ) {
+    state.board = ['', '', '', '', '', '', '', '', ''];
+    state.currentPlayer = 'X';
+    state.turnCount = 0;
+    state.circleWins += 1;
+    render(state);
+  }
+  // 3-4-5
+  else if (
+    state.board[3] === 'X' &&
+    state.board[4] === 'X' &&
+    state.board[5] === 'X'
+  ) {
+    state.board = ['', '', '', '', '', '', '', '', ''];
+    state.currentPlayer = 'X';
+    state.turnCount = 0;
+    state.xWins += 1;
+    render(state);
+  } else if (
+    state.board[3] === 'O' &&
+    state.board[4] === 'O' &&
+    state.board[5] === 'O'
+  ) {
+    state.board = ['', '', '', '', '', '', '', '', ''];
+    state.currentPlayer = 'X';
+    state.turnCount = 0;
+    state.circleWins += 1;
+    render(state);
+  }
+  // 6-7-8
+  else if (
+    state.board[6] === 'X' &&
+    state.board[7] === 'X' &&
+    state.board[8] === 'X'
+  ) {
+    state.board = ['', '', '', '', '', '', '', '', ''];
+    state.currentPlayer = 'X';
+    state.turnCount = 0;
+    state.xWins += 1;
+    render(state);
+  } else if (
+    state.board[6] === 'O' &&
+    state.board[7] === 'O' &&
+    state.board[8] === 'O'
+  ) {
+    state.board = ['', '', '', '', '', '', '', '', ''];
+    state.currentPlayer = 'X';
+    state.turnCount = 0;
+    state.circleWins += 1;
+    render(state);
+  }
+  // 0, 3, 6
+  else if (
+    state.board[0] === 'X' &&
+    state.board[3] === 'X' &&
+    state.board[6] === 'X'
+  ) {
+    state.board = ['', '', '', '', '', '', '', '', ''];
+    state.currentPlayer = 'X';
+    state.turnCount = 0;
+    state.xWins += 1;
+    render(state);
+  } else if (
+    state.board[0] === 'O' &&
+    state.board[3] === 'O' &&
+    state.board[6] === 'O'
+  ) {
+    state.board = ['', '', '', '', '', '', '', '', ''];
+    state.currentPlayer = 'X';
+    state.turnCount = 0;
+    state.circleWins += 1;
+    render(state);
+  }
+  // 1-4-7
+  else if (
+    state.board[1] === 'X' &&
+    state.board[4] === 'X' &&
+    state.board[7] === 'X'
+  ) {
+    state.board = ['', '', '', '', '', '', '', '', ''];
+    state.currentPlayer = 'X';
+    state.turnCount = 0;
+    state.xWins += 1;
+    render(state);
+  } else if (
+    state.board[1] === 'O' &&
+    state.board[4] === 'O' &&
+    state.board[7] === 'O'
+  ) {
+    state.board = ['', '', '', '', '', '', '', '', ''];
+    state.currentPlayer = 'X';
+    state.turnCount = 0;
+    state.circleWins += 1;
+    render(state);
+  }
+  // 2-5-8
+  else if (
+    state.board[2] === 'X' &&
+    state.board[5] === 'X' &&
+    state.board[8] === 'X'
+  ) {
+    state.board = ['', '', '', '', '', '', '', '', ''];
+    state.currentPlayer = 'X';
+    state.turnCount = 0;
+    state.xWins += 1;
+    render(state);
+  } else if (
+    state.board[2] === 'O' &&
+    state.board[5] === 'O' &&
+    state.board[8] === 'O'
+  ) {
+    state.board = ['', '', '', '', '', '', '', '', ''];
+    state.currentPlayer = 'X';
+    state.turnCount = 0;
+    state.circleWins += 1;
+    render(state);
+  }
+  // 0-4-8
+  else if (
+    state.board[0] === 'X' &&
+    state.board[4] === 'X' &&
+    state.board[8] === 'X'
+  ) {
+    state.board = ['', '', '', '', '', '', '', '', ''];
+    state.currentPlayer = 'X';
+    state.turnCount = 0;
+    state.xWins += 1;
+    render(state);
+  } else if (
+    state.board[0] === 'O' &&
+    state.board[4] === 'O' &&
+    state.board[8] === 'O'
+  ) {
+    state.board = ['', '', '', '', '', '', '', '', ''];
+    state.currentPlayer = 'X';
+    state.turnCount = 0;
+    state.circleWins += 1;
+    render(state);
+  }
+  // 2-4-6
+  else if (
+    state.board[2] === 'X' &&
+    state.board[4] === 'X' &&
+    state.board[6] === 'X'
+  ) {
+    state.board = ['', '', '', '', '', '', '', '', ''];
+    state.currentPlayer = 'X';
+    state.turnCount = 0;
+    state.xWins += 1;
+    render(state);
+  } else if (
+    state.board[2] === 'O' &&
+    state.board[4] === 'O' &&
+    state.board[6] === 'O'
+  ) {
+    state.board = ['', '', '', '', '', '', '', '', ''];
+    state.currentPlayer = 'X';
+    state.turnCount = 0;
+    state.circleWins += 1;
+    render(state);
+  } else if (state.turnCount === 9) {
+    state.board = ['', '', '', '', '', '', '', '', ''];
+    state.currentPlayer = 'X';
+    state.turnCount = 0;
+    state.drawCount = state.drawCount + 1;
+    render(state);
+  }
+
+  if (state.xWins === 5) {
+    state.winner = 'X';
+    const displayWinner = document.createElement('h3');
+    displayWinner.classList.add('display-winner-x');
+    displayWinner.innerHTML = `${state.winner} is the winner!`;
+    projectEleme.appendChild(displayWinner);
+    state.xWins = 0;
+    state.circleWins = 0;
+    state.drawCount = 0;
+    render(state);
+  }
+  if (state.circleWins === 5) {
+    state.winner = 'O';
+    const displayWinner = document.createElement('h3');
+    displayWinner.classList.add('display-winner-o');
+    displayWinner.innerHTML = `${state.winner} is the winner!`;
+    projectEleme.appendChild(displayWinner);
+    state.xWins = 0;
+    state.circleWins = 0;
+    state.drawCount = 0;
+    render(state);
+  }
 }
 
 function render(state) {
@@ -65,110 +255,73 @@ function render(state) {
   xWinsElem.innerText = `${state.xWins} wins`;
   drawCountElem.innerText = `${state.drawCount} draws`;
 
+  displayCurrentPlayer.classList.add('displayCurrentPlayer');
+  const slider = document.createElement('span');
+  slider.classList.add('slider');
+  displayCurrentPlayer.appendChild(slider);
+  /* const displayX = document.createElement('h3');
+  displayX.classList.add('displayX');
+  displayX.innerHTML = '<i class="fa-solid fa-x"></i>';
+  const displayCircle = document.createElement('h3');
+  displayCircle.classList.add('displayCircle');
+  displayCircle.innerHTML = '<i class="fa-solid fa-o"></i>';
+  displayCurrentPlayer.appendChild(slider);
+  slider.appendChild(displayX);
+  slider.appendChild(displayCircle); */
+
   clear(boardElem);
   state.board.forEach((_, index) => {
     const cellElement = document.createElement('div');
     cellElement.classList.add('cell');
     cellElement.addEventListener('click', () => nextMove(index));
     boardElem.appendChild(cellElement);
+
+    function nextMove(index) {
+      if (state.board[index] === '') {
+        state.board[index] = state.currentPlayer;
+        state.currentPlayer = state.currentPlayer === 'X' ? 'O' : 'X';
+        state.turnCount = state.turnCount + 1;
+        console.log(state);
+        if (state.board[index] === 'X') {
+          const renderX = document.createElement('h3');
+          renderX.classList.add('renderX');
+          renderX.innerHTML = '<i class="fa-solid fa-x"></i>';
+          cellElement.style.backgroundColor = 'white';
+          cellElement.style.displayFl = '';
+          cellElement.style.cursor = 'default';
+          cellElement.appendChild(renderX);
+        } else if (state.board[index] === 'O') {
+          const renderO = document.createElement('h3');
+          renderO.classList.add('renderO');
+          renderO.innerHTML = '<i class="fa-solid fa-o"></i>';
+          cellElement.style.backgroundColor = 'white';
+          cellElement.style.cursor = 'default';
+          cellElement.appendChild(renderO);
+        }
+        roundWinner();
+      }
+
+      function refreshPage() {
+        state.circleWins = 0;
+        state.xWins = 0;
+        state.drawCount = 0;
+        state.board = ['', '', '', '', '', '', '', '', ''];
+        state.currentPlayer = 'X';
+        state.turnCount = 0;
+        state.winner = '';
+        render(state);
+      }
+
+      refreshButton.addEventListener('click', () => refreshPage());
+    }
   });
 }
 
 render(state);
+console.log(state.board);
 
-/*
-
-const winningCombination = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-
-const winningCombination = [
-  '0,1,2',
-  '3,4,5',
-  '6,7,8',
-  '0,3,6',
-  '1,4,7',
-  '2,5,8',
-  '0,4,8',
-  '2,4,6',
-];
-
-const xTurn = function addX(e) {
-  state.cellElements = e.target;
-  state.cellElements.innerHTML = '<i class="fa-solid fa-x"></i>';
-  state.cellElements.style.backgroundColor = 'white';
-  state.cellElements.style.color = 'blue';
-  state.cellElements.style.fontSize = '55px';
-  state.cellElements.style.cursor = 'default';
-};
-
-const circleTurn = function addCircle(e) {
-  state.cellElements = e.target;
-  state.cellElements.innerHTML = '<i class="fa-solid fa-o"></i>';
-  state.cellElements.style.backgroundColor = 'white';
-  state.cellElements.style.color = 'turquoise';
-  state.cellElements.style.fontSize = '55px';
-  state.cellElements.style.cursor = 'default';
-};
-
-cellElements.forEach((cell) => {
-  cell.addEventListener('click', handleClick, { once: true });
-});
-
-function swapTurns() {
-  circleTurn = !circleTurn;
-}
-
-function isDraw() {
-  return cellElements.every(cell => {
-    return cell.classList.contains(xTurn) ||
-  })
-}
-
-function placeMark(cell, currentClass) {
-  cell.classList.add(currentClass);
-}
-
-function handleClick(e) {
-  const cell = e.target;
-  const currentClass = circleStarts ? xTurn : circleTurn;
-  placeMark(cell, currentClass);
-  if (checkWin(currentClass)) {
-    endGame(false);
-  } else if (isDraw()) {
-    endGame(true);
-  } else {
-    swapTurns();
-  }
-}
-
-function endGame(draw) {
-  if (draw) {
-    state.drawCount++;
-  } else {
-    circleStarts ? state.circleWins++ : state.xWins++;
-  }
-}
-
-function checkWin(currentClass) {
-  return winningCombination.some((combination) => {
-    return combination.every((index) => {
-      return cellElements[index].classList.contains(currentClass);
-    });
-  });
-}
-
-function clear(element) {
-  element.innerHTML = '';
-}
-
-function render(state) {
-  clear(appElement);
+/* if (state.currentPlayer === 'X') {
+  state.displayCurrentPlayer = 'X';
+} else if (state.currentPlayer === 'O') {
+  state.displayCurrentPlayer = 'O';
 } */
