1c52f680a836a521a7f42ad5a83d73243ead21bc
Andreas Ntokas <ntokas.an@gmail.com>
2022-06-11 15:32:48 +0300

    Display current player, local storage & timeout

--- src/app.js
+++ src/app.js
@@ -7,10 +7,11 @@ const xWinsElem = document.getElementById('x-wins');
 const drawCountElem = document.getElementById('draws');
 const boardElem = document.getElementById('board');
 const refreshButton = document.querySelector('.refresh');
+const currentTurnIndicator = document.querySelector('#current-turn-indicator');
 
 // STATE
 
-const state = {
+let state = {
   circleWins: 0,
   xWins: 0,
   drawCount: 0,
@@ -19,7 +20,7 @@ const state = {
   currentPlayer: 'X',
 };
 
-const winningCombination = [
+const winningCombinations = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -35,139 +36,26 @@ function clear(element) {
 }
 
 function findWinner(board) {
-  // 0-1-2
-  if (
-    state.board[0] === 'X' &&
-    state.board[1] === 'X' &&
-    state.board[2] === 'X'
-  ) {
-    return 'X';
-  }
-  if (
-    state.board[0] === 'O' &&
-    state.board[1] === 'O' &&
-    state.board[2] === 'O'
-  ) {
-    return 'O';
-  }
-  // 3-4-5
-  if (
-    state.board[3] === 'X' &&
-    state.board[4] === 'X' &&
-    state.board[5] === 'X'
-  ) {
-    return 'X';
-  }
-  if (
-    state.board[3] === 'O' &&
-    state.board[4] === 'O' &&
-    state.board[5] === 'O'
-  ) {
-    return 'O';
-  }
-  // 6-7-8
-  if (
-    state.board[6] === 'X' &&
-    state.board[7] === 'X' &&
-    state.board[8] === 'X'
-  ) {
-    return 'X';
-  }
-  if (
-    state.board[6] === 'O' &&
-    state.board[7] === 'O' &&
-    state.board[8] === 'O'
-  ) {
-    return 'O';
-  }
-  // 0, 3, 6
-  if (
-    state.board[0] === 'X' &&
-    state.board[3] === 'X' &&
-    state.board[6] === 'X'
-  ) {
-    return 'X';
-  }
-  if (
-    state.board[0] === 'O' &&
-    state.board[3] === 'O' &&
-    state.board[6] === 'O'
-  ) {
-    return 'O';
-  }
-  // 1-4-7
-  if (
-    state.board[1] === 'X' &&
-    state.board[4] === 'X' &&
-    state.board[7] === 'X'
-  ) {
-    return 'X';
-  }
-  if (
-    state.board[1] === 'O' &&
-    state.board[4] === 'O' &&
-    state.board[7] === 'O'
-  ) {
-    return 'O';
-  }
-  // 2-5-8
-  if (
-    state.board[2] === 'X' &&
-    state.board[5] === 'X' &&
-    state.board[8] === 'X'
-  ) {
-    return 'X';
-  }
-  if (
-    state.board[2] === 'O' &&
-    state.board[5] === 'O' &&
-    state.board[8] === 'O'
-  ) {
-    return 'O';
-  }
-  // 0-4-8
-  if (
-    state.board[0] === 'X' &&
-    state.board[4] === 'X' &&
-    state.board[8] === 'X'
-  ) {
-    return 'X';
-  }
-  if (
-    state.board[0] === 'O' &&
-    state.board[4] === 'O' &&
-    state.board[8] === 'O'
-  ) {
-    return 'O';
-  }
-  // 2-4-6
-  if (
-    state.board[2] === 'X' &&
-    state.board[4] === 'X' &&
-    state.board[6] === 'X'
-  ) {
-    return 'X';
-  }
-  if (
-    state.board[2] === 'O' &&
-    state.board[4] === 'O' &&
-    state.board[6] === 'O'
-  ) {
-    return 'O';
-  }
-  if (
-    state.board[0] !== '' &&
-    state.board[1] !== '' &&
-    state.board[2] !== '' &&
-    state.board[3] !== '' &&
-    state.board[4] !== '' &&
-    state.board[5] !== '' &&
-    state.board[6] !== '' &&
-    state.board[7] !== '' &&
-    state.board[8] !== ''
-  ) {
-    return 'Draw';
-  }
+  const winner = winningCombinations.reduce((acc, curr) => {
+    // Check if we found the winner already.
+    if (acc) return acc;
+    // Try to find winner.
+    if (
+      state.board[curr[0]] === state.board[curr[1]] &&
+      state.board[curr[1]] === state.board[curr[2]]
+    ) {
+      return state.board[curr[0]];
+    }
+  }, null);
+
+  if (winner) return winner;
+
+  // Find if we have a draw.
+  const isDraw = board.findIndex((v) => v === '') === -1;
+
+  if (isDraw) return 'Draw';
+
+  return null;
 }
 
 function refreshPage() {
@@ -177,56 +65,70 @@ function refreshPage() {
   state.board = ['', '', '', '', '', '', '', '', ''];
   state.currentPlayer = 'X';
   state.startingPlayer = 'X';
+
+  localStorage.setItem('gameData', JSON.stringify(state));
+
   render(state);
 }
 
 function changeOrder() {
   state.startingPlayer = state.startingPlayer === 'X' ? 'O' : 'X';
   state.currentPlayer = state.startingPlayer;
-  render(state);
-  console.log(state.startingPlayer);
 }
 
 function swapPlayer() {
-  if (state.startingPlayer === 'X') {
-    state.currentPlayer =
-      state.currentPlayer === state.startingPlayer ? 'O' : 'X';
-    render(state);
-  }
-  if (state.startingPlayer === 'O') {
-    state.currentPlayer =
-      state.currentPlayer === state.startingPlayer ? 'X' : 'O';
-    render(state);
-  }
+  state.currentPlayer = state.currentPlayer === 'X' ? 'O' : 'X';
 }
 
 function nextMove(index) {
   const hasEmptySlot = state.board.some((item) => item === '');
 
-  if (hasEmptySlot === true) {
+  if (hasEmptySlot) {
     state.board[index] = state.currentPlayer;
     swapPlayer();
+    localStorage.setItem('gameData', JSON.stringify(state));
     render(state);
   }
+
   if (findWinner(state.board) === 'X') {
-    state.board = ['', '', '', '', '', '', '', '', ''];
-    state.xWins += 1;
-    changeOrder();
-    render(state);
+    setTimeout(() => {
+      state.board = ['', '', '', '', '', '', '', '', ''];
+      state.xWins += 1;
+      changeOrder();
+      localStorage.setItem('gameData', JSON.stringify(state));
+      render(state);
+    }, 100);
   }
+
   if (findWinner(state.board) === 'O') {
-    state.board = ['', '', '', '', '', '', '', '', ''];
-    state.circleWins += 1;
-    changeOrder();
-    render(state);
+    setTimeout(() => {
+      state.board = ['', '', '', '', '', '', '', '', ''];
+      state.circleWins += 1;
+      changeOrder();
+      localStorage.setItem('gameData', JSON.stringify(state));
+      render(state);
+    }, 100);
   }
+
   if (findWinner(state.board) === 'Draw') {
-    state.board = ['', '', '', '', '', '', '', '', ''];
-    state.drawCount += 1;
-    changeOrder();
-    render(state);
+    setTimeout(() => {
+      state.board = ['', '', '', '', '', '', '', '', ''];
+      state.drawCount += 1;
+      changeOrder();
+      localStorage.setItem('gameData', JSON.stringify(state));
+      render(state);
+    }, 100);
   }
 }
+
+window.addEventListener('load', () => {
+  state =
+    localStorage.getItem('gameData') !== null
+      ? JSON.parse(localStorage.getItem('gameData'))
+      : state;
+
+  render(state);
+});
 
 function render(state) {
   circleWinsElem.innerText = `${state.circleWins} wins`;
@@ -234,27 +136,36 @@ function render(state) {
   drawCountElem.innerText = `${state.drawCount} draws`;
 
   clear(boardElem);
-  console.log(state);
+
   state.board.forEach((_, index) => {
     const cellElement = document.createElement('div');
     cellElement.classList.add('cell');
+
     if (state.board[index] === '') {
       cellElement.addEventListener('click', () => nextMove(index));
     }
+
     if (state.board[index] === 'X') {
       const renderX = document.createElement('h3');
+
       renderX.classList.add('renderX');
       renderX.innerHTML = '<i class="fa-solid fa-x"></i>';
+
       cellElement.style.backgroundColor = 'white';
-      cellElement.style.displayFl = '';
       cellElement.style.cursor = 'default';
+
       cellElement.appendChild(renderX);
-    } else if (state.board[index] === 'O') {
+    }
+
+    if (state.board[index] === 'O') {
       const renderO = document.createElement('h3');
+
       renderO.classList.add('renderO');
       renderO.innerHTML = '<i class="fa-solid fa-o"></i>';
+
       cellElement.style.backgroundColor = 'white';
       cellElement.style.cursor = 'default';
+
       cellElement.appendChild(renderO);
     }
 
@@ -262,6 +173,27 @@ function render(state) {
 
     refreshButton.addEventListener('click', () => refreshPage());
   });
+  // Render current-turn indicator.
+
+  clear(currentTurnIndicator);
+
+  const xTurn = document.createElement('div');
+  const oTurn = document.createElement('div');
+
+  if (state.currentPlayer === 'X') {
+    xTurn.classList.add('xTurnActive');
+    xTurn.innerHTML = '<i class="fa-solid fa-x"></i>';
+    oTurn.classList.add('oTurn');
+    oTurn.innerHTML = '<i class="fa-solid fa-o"></i>';
+  } else {
+    xTurn.classList.add('xTurn');
+    xTurn.innerHTML = '<i class="fa-solid fa-x"></i>';
+    oTurn.classList.add('oTurnActive');
+    oTurn.innerHTML = '<i class="fa-solid fa-o"></i>';
+  }
+
+  currentTurnIndicator.appendChild(xTurn);
+  currentTurnIndicator.appendChild(oTurn);
 }
 
 render(state);
